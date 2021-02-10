de09e4d513c4857d164cd656ab50fa3293b7a161
Igor Suvorov <hi@isuvorov.com>
2021-02-10 19:19:56 +0200

    fix: copy path fix

--- packages/cli-scripts/scripts/run/update-starter-kit.js
+++ packages/cli-scripts/scripts/run/update-starter-kit.js
@@ -4,10 +4,7 @@ const { run, shell, findPath } = require('@lskjs/cli/utils');
 const findExternal = (name) => findPath({ name, local: 0 });
 
 const main = async () => {
-  // await shell(`rm -rf scripts/assets scripts/opt scripts/package scripts/run scripts/utils scripts/update-starter-kit`);
-  await shell(
-    `rsync -aEp --exclude-from='${findPath('scripts/hooks/update-starter-kit-exclude.txt')}' ${findExternal('')}/ .`,
-  );
+  const filesPath = findExternal('files');
   const lightCopyFiles = [
     '.lskjs.js',
     '.all-contributorsrc',
@@ -15,11 +12,11 @@ const main = async () => {
     'docker-compose.yml',
     '.gitlab.ci.yml',
     '.github',
-  ]
-    .map(findExternal)
-    .join(' ');
-  // await shell(`rsync -aEp --ignore-existing ${lightCopyFiles} .`);
-  // await shell(`rsync -aEp ${findExternal('scripts/assets')}/ ./scripts/assets`);
+  ];
+  const exclude = lightCopyFiles.map((file) => `--exclude ${file}`).join(' ');
+  const copy = lightCopyFiles.map((file) => `${filesPath}/${file}`).join(' ');
+  await shell(`rsync -aEp ${exclude} ${filesPath}/ .`);
+  await shell(`rsync -aEp --ignore-existing ${copy} .`);
 
   // eslint-disable-next-line no-console
   console.log(`
