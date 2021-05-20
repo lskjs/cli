639aa4867f66a82ba0af18718cfeddce1dfeb148
Igor Suvorov <hi@isuvorov.com>
2021-05-20 10:48:30 +0300

    fix: bootstrap bug

--- packages/cli-scripts/scripts/run/bootstrap.js
+++ packages/cli-scripts/scripts/run/bootstrap.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-const { shell, run, getLskConfig, shellLerna } = require('@lskjs/cli-utils');
+const { shell, run, getLskConfig, lerna } = require('@lskjs/cli-utils');
 
 const main = async () => {
   const config = getLskConfig();
   if (config && config.storybook) {
     await shell('lsk run bootstrap:storybook');
   }
-  await shellLerna(`exec --parallel --no-prefix -- lsk run bootstrap`);
+  await lerna(`exec --parallel --no-prefix -- lsk run bootstrap`);
 };
 
 run(main);
