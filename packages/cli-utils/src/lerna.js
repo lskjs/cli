f06f17f5c4cf407f3abe3d02e749eafc302d732b
Igor Suvorov <hi@isuvorov.com>
2021-05-20 10:54:36 +0300

    fix: bootstrap bug

--- packages/cli-utils/src/lerna.js
+++ packages/cli-utils/src/lerna.js
@@ -1,7 +1,7 @@
 const { shell } = require('./shell');
 const { findBin } = require('./findBin');
 
-const lerna = (...args) => shell([findBin('lerna'), '--loglevel warn', ...args].join(''));
+const lerna = (...args) => shell([findBin('lerna'), '--loglevel warn', ...args].join(' '));
 
 module.exports = {
   lerna,
