d7b5181616b61d8e14b533996c76942cde898ee4
Igor Suvorov <hi@isuvorov.com>
2021-05-27 12:49:19 +0300

    fix: lerna use global scope variable

--- packages/cli-utils/src/lerna.js
+++ packages/cli-utils/src/lerna.js
@@ -2,7 +2,7 @@ const { shell } = require('./shell');
 const { findBin } = require('./findBin');
 
 const lerna = (...args) => {
-  const scope = !!process.env.SCOPE || !!process.env.PACKAGES || null;
+  const scope = process.env.SCOPE || process.env.PACKAGES || null;
   shell([findBin('lerna'), '--loglevel warn', scope && `--scope ${scope}`, ...args].filter(Boolean).join(' '));
 };
 
