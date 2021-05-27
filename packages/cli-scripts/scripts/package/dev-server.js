fef8fd9c6028fc2e27b0deb7b44ba66d7cf2e822
Igor Suvorov <hi@isuvorov.com>
2021-05-27 09:24:22 +0300

    feat: lerna use global scope variable

--- packages/cli-scripts/scripts/package/dev-server.js
+++ packages/cli-scripts/scripts/package/dev-server.js
@@ -3,7 +3,7 @@
 const { run, shell, findBin } = require('@lskjs/cli-utils');
 
 const main = async () => {
-  await shell(`${findBin('nodemon')} --delay 0.5 index.server.js`);
+  await shell(`${findBin('nodemon')} --inspect --delay 0.5 index.server.js`);
 };
 
 run(main);
--- packages/cli-utils/src/lerna.js
+++ packages/cli-utils/src/lerna.js
@@ -1,7 +1,10 @@
 const { shell } = require('./shell');
 const { findBin } = require('./findBin');
 
-const lerna = (...args) => shell([findBin('lerna'), '--loglevel warn', ...args].join(' '));
+const lerna = (...args) => {
+  const scope = !!process.env.SCOPE || !!process.env.PACKAGES || null;
+  shell([findBin('lerna'), '--loglevel warn', scope && `--scope ${scope}`, ...args].filter(Boolean).join(' '));
+};
 
 module.exports = {
   lerna,
