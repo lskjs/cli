9a61c54f7201cf71f77f588927c4249f7bf880f0
Igor Suvorov <hi@isuvorov.com>
2022-03-07 11:52:42 +0300

    feat: package updates

---
+++ scripts/reupdate.sh
@@ -0,0 +1,25 @@
+cd . && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+cd packages/cli && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+cd ../../packages/cli-utils && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+cd ../../packages/cli-scripts && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+cd ../../../packages/cli-scripts/files && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+cd  ../../../packages/cli-scripts/files && \
+rm -rf package-lock.json && \
+rm -rf node_modules && \
+npm i && \
+echo "OK"
