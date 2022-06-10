2fe78d27120b9e18e17ed667dd548f9eb5d8659f
Igor Suvorov <hi@isuvorov.com>
2022-06-10 18:53:07 +0300

    fix: find jest

--- packages/cli-scripts/scripts/package/test.js
+++ packages/cli-scripts/scripts/package/test.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-const { run, shell } = require('@lskjs/cli-utils');
+const { run, shell, findBin } = require('@lskjs/cli-utils');
 
 const main = async () => {
   const argv = process.argv.slice(2);
-  // console.log({argv})
-  // const argv = process.argv.slice(2);
-  await shell(`jest --passWithNoTests ${argv.join(' ')}`);
+  await shell(`${findBin('jest')} --passWithNoTests ${argv.join(' ')}`);
 };
 
 run(main);
